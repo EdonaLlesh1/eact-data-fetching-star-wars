1169cd5f61808a5c3afd0a580add803e07006123
Edona Lleshi <edona.lleshi95@gmail.com>
2024-01-19 12:30:10 +0100

    add

--- pages/characters/1.js
+++ pages/characters/1.js
@@ -1,17 +1,34 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
 
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error ("Failed to fetch data");
+  }
+  return response.json();
+};
+
 export default function Character() {
-  const id = 1;
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, error } = useSWR(id ? `https://swapi.dev/api/people/${id}` : null, fetcher);
+
+  if (error) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <Layout>
       <Card
-        id={id}
-        name={"Luke Skywalker"}
-        height={172}
-        eyeColor={"blue"}
-        birthYear={"19BBY"}
+        id={data.id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
       />
     </Layout>
   );
